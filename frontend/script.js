971ad450a36c3097d3e612e531bdcb72ff2af7a4
Laya Pandey <layapandey7171@gmail.com>
2025-07-03 08:02:04 +0545

    Front page set

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: { delay: 3000 },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
